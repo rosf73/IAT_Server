f630a6628acdc6428bd3305e3e3d9b6a8e013f9a
rosf73 <47631768+rosf73@users.noreply.github.com>
2020-09-27 23:20:06 +0900

    환경 변수 파일 등록

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const app = express()
-const port = 3000
+
+dotenv.config(); // .env(환경 변수 파일)를 환경변수에 설정 
+
+const port = process.env.PORT; // 직접 지정한 환경변수 이용
 
 app.get('/', (req, res) => {
   res.send("Hello, World!");

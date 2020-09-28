b236080435fa8fef9f6cefc3c553d8e1e19611f1
rosf73 <47631768+rosf73@users.noreply.github.com>
2020-09-28 15:44:56 +0900

    SQLite 연결 테스트

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const dotenv = require('dotenv')
+const sqlite = require('sqlite3').verbose()
 const express = require('express')
+const { dropQuery, insertQuery, dummyDataQuery, selectAllQuery } = require('./query')
+
 const app = express()
+
+const db = new sqlite.Database('./db/myDB.db', sqlite.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log('Connected to the mydb database.');
+  }
+});
+
+db.serialize(() => {
+  db.each(dropQuery);
+  db.each(insertQuery);
+  db.each(dummyDataQuery);
+  db.each(selectAllQuery, (err, row) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(row.phone_num + ' ' + row.password);
+  });
+});
 
 dotenv.config(); // .env(환경 변수 파일)를 환경변수에 설정 
 
 const port = process.env.PORT; // 직접 지정한 환경변수 이용
 
 app.get('/', (req, res) => {
+  // const query = `SELECT * FROM person`;
+  // db.serialize();
+  // db.all(query, (err, row) => {
+  //   res.render('show', {data : row});
+  // });
   res.send("Hello, World!");
 });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
-})
+});
+
+db.close((err) => {
+  if (err) {
+      console.error(err.message);
+  } else {
+      console.log('Close the database connection.');
+  }
+});

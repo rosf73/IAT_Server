c1bdd5d2c29bcf69f9f2726655927bb20fb1c508
rosf73 <47631768+rosf73@users.noreply.github.com>
2020-09-29 16:33:21 +0900

    라우팅 테스트 및 db 클로징 업데이트

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const sqlite = require('sqlite3').verbose()
 const express = require('express')
 
-const { dropQuery, insertQuery, dummyDataQuery, selectAllQuery } = require('./query')
+const { selectAllQuery } = require('./query')
 require('./env')
 
-const app = express()
 
+const app = express()
 const db = new sqlite.Database('./db/myDB.db', sqlite.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
@@ -14,37 +14,36 @@ const db = new sqlite.Database('./db/myDB.db', sqlite.OPEN_READWRITE, (err) => {
   }
 });
 
-db.serialize(() => {
-  db.each(dropQuery);
-  db.each(insertQuery);
-  db.each(dummyDataQuery);
+const port = process.env.PORT; // 직접 지정한 환경변수 이용
+
+app.get('/', (req, res) => {
+  db.serialize();
   db.each(selectAllQuery, (err, row) => {
     if (err) {
       console.error(err.message);
     }
-    console.log(row.phone_num + ' ' + row.password);
+    console.log(row.phone_num);
   });
+  res.send("Hello, World! get");
 });
 
-const port = process.env.PORT; // 직접 지정한 환경변수 이용
-
-app.get('/', (req, res) => {
-  // const query = `SELECT * FROM person`;
-  // db.serialize();
-  // db.all(query, (err, row) => {
-  //   res.render('show', {data : row});
-  // });
-  res.send("Hello, World!");
+app.post('/data', (req, res) => {
+  console.log("hi");
+  res.send("Hello, World! post");
 });
 
-app.listen(port, () => {
+var server = app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 });
 
-db.close((err) => {
-  if (err) {
+// 서버 종료 시 db close 및 server close
+process.on('SIGINT', () => {
+  server.close();
+  db.close((err) => {
+    if (err) {
       console.error(err.message);
-  } else {
+    } else {
       console.log('Close the database connection.');
-  }
+    }
+  });
 });

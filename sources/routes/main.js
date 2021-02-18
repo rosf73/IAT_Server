a14f06cd5967401b8a948c4b69e8c85870b4ef6d
rosf73 <alswlzhsclq@naver.com>
2021-02-18 21:59:30 +0900

    데이터 정제 문제 해결을 위한 관리자페이지 차단

--- sources/routes/main.js
+++ sources/routes/main.js
@@ -4,86 +4,87 @@ const crypto = require('crypto')
 const db = require('../../db')
 const template = require('../statics/template')
 
-router.get("/", async (req, res) => {
-  const questions = await db.query(`SELECT type, sub_content FROM question ORDER BY question_id ASC`, []);
-  const answers = await db.query(`SELECT day, answer_data, phone_num FROM answer ORDER BY answer_id ASC`, []);
-  const data = await db.query(`SELECT * FROM data ORDER BY data_id ASC`, []);
-  const map_answers = await Promise.all(
-    answers.map(async elem => {
-      const user = await db.query(`SELECT * FROM participant WHERE phone_num = '${elem.phone_num}'`, []);
+router.get("/", (req, res) => {
+//router.get("/", async (req, res) => {
+  //const questions = await db.query(`SELECT type, sub_content FROM question ORDER BY question_id ASC`, []);
+  //const answers = await db.query(`SELECT day, answer_data, phone_num FROM answer ORDER BY answer_id ASC`, []);
+  //const data = await db.query(`SELECT * FROM data ORDER BY data_id ASC`, []);
+  //const map_answers = await Promise.all(
+    //answers.map(async elem => {
+      //const user = await db.query(`SELECT * FROM participant WHERE phone_num = '${elem.phone_num}'`, []);
 
-      const participants = await db.query(`SELECT phone_num, progress, iv FROM participant`, []);
-      let participant = await Promise.all(
-        participants.filter(e => elem.phone_num === e.phone_num)
-      );
-      participant = await Promise.all(
-        participant.map(e => {
-          const decipher = crypto.createDecipheriv('aes-256-cbc', process.env.CIPHER_KEY, Buffer.from(e.iv, 'hex'));
-          let result = decipher.update(e.phone_num, 'base64', 'utf8');
-          result += decipher.final('utf8');
+      //const participants = await db.query(`SELECT phone_num, progress, iv FROM participant`, []);
+      //let participant = await Promise.all(
+        //participants.filter(e => elem.phone_num === e.phone_num)
+      //);
+      //participant = await Promise.all(
+        //participant.map(e => {
+          //const decipher = crypto.createDecipheriv('aes-256-cbc', process.env.CIPHER_KEY, Buffer.from(e.iv, 'hex'));
+          //let result = decipher.update(e.phone_num, 'base64', 'utf8');
+          //result += decipher.final('utf8');
     
-          e.phone_num = result;
-          return e;
-        })
-      );
+          //e.phone_num = result;
+          //return e;
+        //})
+      //);
       
-      elem.phone_num = participant[0].phone_num;
-      elem.age = user[0].age;
-      elem.gender = user[0].gender;
-      elem.major = user[0].major;
-      elem.grade = user[0].grade;
-      elem.meeting = user[0].meeting;
-      return elem;
-    })
-  );
-  const map_data = await Promise.all(
-    data.map(async elem => {
-      const sub_question = await db.query(`SELECT * FROM sub_question WHERE sub_question_id = ${elem.sub_question_id}`, []);
+      //elem.phone_num = participant[0].phone_num;
+      //elem.age = user[0].age;
+      //elem.gender = user[0].gender;
+      //elem.major = user[0].major;
+      //elem.grade = user[0].grade;
+      //elem.meeting = user[0].meeting;
+      //return elem;
+    //})
+  //);
+  //const map_data = await Promise.all(
+    //data.map(async elem => {
+      //const sub_question = await db.query(`SELECT * FROM sub_question WHERE sub_question_id = ${elem.sub_question_id}`, []);
       
-      const participants = await db.query(`SELECT phone_num, progress, iv FROM participant`, []);
-      let participant = await Promise.all(
-        participants.filter(e => elem.phone_num === e.phone_num)
-      );
-      participant = await Promise.all(
-        participant.map(e => {
-          const decipher = crypto.createDecipheriv('aes-256-cbc', process.env.CIPHER_KEY, Buffer.from(e.iv, 'hex'));
-          let result = decipher.update(e.phone_num, 'base64', 'utf8');
-          result += decipher.final('utf8');
+      //const participants = await db.query(`SELECT phone_num, progress, iv FROM participant`, []);
+      //let participant = await Promise.all(
+        //participants.filter(e => elem.phone_num === e.phone_num)
+      //);
+      //participant = await Promise.all(
+        //participant.map(e => {
+          //const decipher = crypto.createDecipheriv('aes-256-cbc', process.env.CIPHER_KEY, Buffer.from(e.iv, 'hex'));
+          //let result = decipher.update(e.phone_num, 'base64', 'utf8');
+          //result += decipher.final('utf8');
     
-          e.phone_num = result;
-          return e;
-        })
-      );
+          //e.phone_num = result;
+          //return e;
+        //})
+      //);
 
-      elem.phone_num = participant[0].phone_num;
-      elem.question = sub_question[0].question_id + "_" + sub_question[0].number;
-      return elem;
-    })
-  );
+      //elem.phone_num = participant[0].phone_num;
+      //elem.question = sub_question[0].question_id + "_" + sub_question[0].number;
+      //return elem;
+    //})
+  //);
 
   var q_str = "";
-  for (var i = 0; i < questions.length; i++) {
-    if (questions[i].sub_content === 0) {
-      q_str += (i+1)+",";
-    }
-    else for (var j = 0; j < questions[i].sub_content; j++) {
-      if (questions[i].type === 4) // IAT
-        q_str += (i+1)+"_("+(j+1)+")_D,";
-      else
-        q_str += (i+1)+"_("+(j+1)+"),";
-    }
-  }
+  //for (var i = 0; i < questions.length; i++) {
+    //if (questions[i].sub_content === 0) {
+      //q_str += (i+1)+",";
+    //}
+    //else for (var j = 0; j < questions[i].sub_content; j++) {
+      //if (questions[i].type === 4) // IAT
+        //q_str += (i+1)+"_("+(j+1)+")_D,";
+      //else
+        //q_str += (i+1)+"_("+(j+1)+"),";
+    //}
+  //}
 
-  var a_str = "";
-  for (var i = 0; i < map_answers.length; i++) {
-    a_str += map_answers[i].answer_data;
-    a_str += map_answers[i].age + "," + map_answers[i].gender + "," + map_answers[i].major + "," + map_answers[i].grade + "," + map_answers[i].day + "," + map_answers[i].phone_num.substring(0, 3) + "-" + map_answers[i].phone_num.substring(3) + "," + map_answers[i].meeting + "\n";
-  }
+  var a_str = ",,\n";
+  //for (var i = 0; i < map_answers.length; i++) {
+    //a_str += map_answers[i].answer_data;
+    //a_str += map_answers[i].age + "," + map_answers[i].gender + "," + map_answers[i].major + "," + map_answers[i].grade + "," + map_answers[i].day + "," + map_answers[i].phone_num.substring(0, 3) + "-" + map_answers[i].phone_num.substring(3) + "," + map_answers[i].meeting + "\n";
+  //}
 
   var d_str = "step,반응 시간(ms),z점수\n";
-  for (var i = 0; i < map_data.length; i++) {
-    d_str += map_data[i].phone_num.substring(0, 3) + "-" + map_data[i].phone_num.substring(3) + "," + map_data[i].question + "\n" + map_data[i].test_data;
-  }
+  //for (var i = 0; i < map_data.length; i++) {
+    //d_str += map_data[i].phone_num.substring(0, 3) + "-" + map_data[i].phone_num.substring(3) + "," + map_data[i].question + "\n" + map_data[i].test_data;
+  //}
   
   var title = '관리자 페이지 입니다.';
   var html = template.MAIN_HTML(title, `
